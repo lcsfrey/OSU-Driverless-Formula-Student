3d1c98b46327cddb2e2be7ee1a09c1cf8e991a01
Korsness <thomas.korsness@global-formula-racing.com>
2019-04-15 12:33:52 -0700

    Added EKF

---
+++ ros_ws/src/SEE/see_localization/scripts/EKF_Localization.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import rospy
+import message_filters
+import numpy as np
+from see_ego_motion.msg import see_ego_motion_interface
+from see_localization.msg import vehicle_pose_msg
+
+
+def motionModel(pre_state,v,w,time):
+	_theta = pre_state[2,0]
+	_delta_x = -v/w * np.sin(_theta) + v/w * np.sin(_theta + (w * time))
+	_delta_y = v/w * np.cos(_theta) - v/w * np.cos(_theta + (w * time))
+	_delta_theta = w
+	new_state = pre_state + np.array([[_delta_x],[_delta_y],[_delta_theta]])
+
+	return new_state
+
+def Jacobian(stateVector,v,w,time):
+	_theta = stateVector[2,0]
+	_Jacobian = np.array([[1,0,-v/w * np.cos(_theta) + v/w * np.cos(_theta + (w * time))],
+						  [0,1,-v/w + np.sin(_theta) + v/w + np.sin(_theta + (w * time))],
+						  [0,0,1]])
+	return _Jacobian
+
+
+def EKF(pre_state,pre_cov,v,w,time,delta_state):
+	new_state = motionModel(pre_state,v,w,time)
+	F = Jacobian(pre_state,v,w,time)
+	new_cov = np.dot(F,pre_cov).dot(F.T) + np.eye(3)
+
+	Q = np.eye(3)
+
+	jH = np.eye(3)
+
+	z_est = delta_state
+
+	y = z_est - new_state
+
+	S = jH.dot(new_cov).dot(jH.T) + Q
+
+	K = new_cov.dot(jH.T).dot(np.linalg.inv(S))
+
+	new_state = new_state + K.dot(y)
+
+	new_cov = (np.eye(new_state.shape[0]) - K.dot(jH)).dot(new_cov)
+
+	return new_state, new_cov
+
+
+
+
+def callback(see_ego_motion_interface):
+
+	v_tan = see_ego_motion_interface.velocity_longtitudonal
+	v_nor = see_ego_motion_interface.velocity_lateral
+	w = see_ego_motion_interface.yaw_rate
+
+	x_global = see_ego_motion_interface.x_global
+	y_global = see_ego_motion_interface.y_global
+	theta_global = see_ego_motion_interface.theta_global
+
+	delta_t = see_ego_motion_interface.delta_time
+
+	v = np.sqrt(v_tan ** 2 + v_nor ** 2)
+
+	state = np.array([[0],
+					  [0],
+					  [0]]) #initial state subject to change, haven't figure out how to get this one
+	cov = np.array([1,0,0],
+				   [0,1,0],
+				   [0,0,1])
+
+
+	delta_state = np.array([[x_global],
+							[y_global],
+							[theta_global]])
+
+	state, cov = EKF(state,cov,v,w,delta_t,delta_state)
+
+	rospy.loginfo("state:[]".format(state))
+
+	covMsg = [cov[0,0],cov[0,1],cov[0,2],cov[1,0],cov[1,1],cov[1,2],cov[2,0],cov[2,1],cov[2,2]]
+
+	pub = ros.py.Publisher("/see_localization/Vehicle_pose", vehicle_pose_msg)
+	msg = vehicle_pose_msg
+	msg.x_global = state[0,0]
+	msg.y_global = state[1,0]
+	msg.theta_global = state[2,0]
+	msg.corvariance = covMsg
+	pub.publish(msg)
+
+
+
+	return
+
+
+def main():
+	rospy.init_node("EKF_localization")
+	'''dynamics_sub = message_filters.Subscriber("dynamics", Dynamics)
+	delta_sub = message_filters.Subscriber("delta", OdomDeltas)
+	delta_t = message_filters.Subscriber("delta_t", see_ego_motion_interface)
+
+
+	ts = message_filters.TimeSynchronizer([dynamics_sub,delta_sub,delta_t],10)
+	ts.registerCallback(callback)'''
+
+	rospy.Subscriber("/see_localization/OdomSubscriber", see_ego_motion_interface,callback)
+
+	rospy.spin()
+
+	return
+
+if __name__ == '__main__':
+	main()
